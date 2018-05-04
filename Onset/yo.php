c69600bc8fde038b669e155977f544b34e92ffd2
Zubeida Casmod Dawood (Khan) <zubzzz@hotmail.com>
2018-05-04 10:52:27 +0200

    Update yo.php

--- Onset/yo.php
+++ Onset/yo.php
@@ -1,8 +1,8 @@
 <?php
 $yolo = "Coonected to database."
-mysql_connect("127.3.143.1:3306","admint2HhvWi","2iAB21BlvIii") or die (mysql_error());
+mysql_connect("","","") or die (mysql_error());
 echo $yolo;
-mysql_select_db("ONSETData");
+mysql_select_db("");
 
 
 ?>
